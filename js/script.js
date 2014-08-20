c12252793c54c75de2167e4bfe65d00155e4df65
JakeSidSmith <jake@dabapps.com>
2014-08-20 14:12:10 +0100

    Jumpy demo

--- js/script.js
+++ js/script.js
@@ -12,7 +12,12 @@ $(document).ready(function () {
       .rotateY(1)
       .translateY(Math.cos(yRotation / 360 * 25 / 4) * 100)
       .translateX(Math.sin(yRotation / 360 * 25 / 4) * 100)
-      .width(Math.max(Math.abs(Math.cos(yRotation / 360 * 25 / 4) * 200), 100))
-      .height(Math.max(Math.abs(Math.cos(yRotation / 360 * 25 / 8) * 400), 100));
+      //.width(Math.max(Math.abs(Math.cos(yRotation / 360 * 25 / 4) * 200), 100))
+      //.height(Math.max(Math.abs(Math.cos(yRotation / 360 * 25 / 8) * 400), 100));
+      .size(
+        yRotation % 10 === 0 ? Math.max(Math.random(), 0.2) * 100 : undefined,
+        yRotation % 10 === 0 ? Math.max(Math.random(), 0.2) * 100 : undefined,
+        yRotation % 10 === 0 ? Math.max(Math.random(), 0.2) * 100 : undefined
+      );
   }, 1000/60);
 });
